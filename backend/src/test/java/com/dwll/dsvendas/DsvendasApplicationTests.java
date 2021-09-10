44224cb02d1779fc1599a56f4333ebc97c9da392
wslmacieira <wslmacieira@outlook.com>
2021-09-10 18:52:35 -0300

    Project created

---
+++ backend/src/test/java/com/dwll/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwll.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
